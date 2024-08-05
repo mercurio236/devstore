8ea97db0a0738defe97ec67f7c5b9a734b27732a
Arley Souto <macx15@hotmail.com>
2024-08-04 23:06:31 -0300

    feat: header criado

---
+++ src/app/(store)/(home)/loading.tsx
@@ -0,0 +1,5 @@
+export default async function HomeLoading(){
+    return(
+        <p>Carregando...</p>
+    )
+}
---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react'
+import Header from '../components/header'
+
+export default function StoreLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 px-8 p-8">
+      <Header />
+      {children}
+    </div>
+  )
+}
