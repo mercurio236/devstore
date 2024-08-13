dc957d522b025a5cd05a423d01a0327ed0b98466
Arley Souto <macx15@hotmail.com>
2024-08-12 21:34:25 -0300

    feat: fetch de dados no produto

---
+++ src/app/api/products/[slug]/route.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod'
+import data from '../data.json'
+import { NextResponse } from 'next/server'
+
+export async function GET(
+  _: Request,
+  { params }: { params: { slug: string } }
+) {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  const slug = z.string().parse(params.slug) //verifica se é uma string, evita o undefined
+
+  const product = data.products.find((product) => product.slug === slug)
+
+  if (!product) {
+    return Response.json(
+      {
+        message: 'Product not found.',
+      },
+      { status: 400 }
+    )
+  }
+
+  return Response.json(product)
+}
